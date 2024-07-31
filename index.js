06efaeb13bc6f1f9c8c289fbd4c49fdd27a97ce3
Grace Nieboer <grace.nieboer@outlook.com>
2024-07-30 22:28:18 -0400

    add the boilerplate code

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 const PORT = 8000
 const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+// morgan will help debug the backend a little better. It is a logger
+const fetch = require('node-fetch')
+
+require('dotenv').config()
+// per the usage docs
+
+morgan('tiny')
+// per the docs
+
+const app = express()
+
+app.use(morgan('tiny'))
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
