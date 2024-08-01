dcc9a64e8ca33bc9a9b0dc8f86eaf204e74096ef
Grace Nieboer <grace.nieboer@outlook.com>
2024-07-31 22:23:48 -0400

    configure tailwind and add some basic styling to the restaurant feed, and implement tertiary statement for the boolean value

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -2,10 +2,10 @@ import { useState } from "react";
 
 export const Restuarant = ({restaurant}) => {
     return(
-        <div>
-            <div>{restaurant.name}</div>
+        <div className="p-10 m-2 border-2 border-orange-700 rounded-full">
+            <div className="font-black text-orange-800" >{restaurant.name}</div>
             <div>{restaurant.description}</div>
-            <div>Have I been there yet? {restaurant.visited}</div>
+            <div>Have I been there yet? {restaurant.visited? `yes!` : `not yet`}</div>
 
         </div>
     )
