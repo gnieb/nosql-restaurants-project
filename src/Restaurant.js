dfe35476c80506c0ad1658cf058f86780f5ac177
Grace Nieboer <grace.nieboer@outlook.com>
2024-08-03 23:12:21 -0400

    add state for pizzaz to test Jest

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -1,11 +1,25 @@
 import { useState } from "react";
 
 export const Restuarant = ({restaurant}) => {
+    const [pizzaz, setPizzaz ] = useState(false);
+
+    const onMouseEnter = () => {
+        setPizzaz(true);
+      };
+    
+      const onMouseLeave = () => {
+        setPizzaz(false);
+      };
+
+
     return(
         <div className="p-10 m-2 border-2 border-orange-700 rounded-full">
             <div className="font-black text-orange-800" >{restaurant.name}</div>
             <div>{restaurant.description}</div>
-            <div>Have I been there yet? {restaurant.visited? `yes!` : `not yet`}</div>
+            <div 
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave} 
+            >Have I been there yet? {restaurant.visited? `yes!` : `not yet`}</div>
 
         </div>
     )
